6cbd2014c21d0945b40465f4ecee22360f5da606
chananporn04 <67319010004@TNK.ac.th>
2024-10-30 17:34:18 +0700

    004

---
+++ week3.py/area.py
@@ -0,0 +1,17 @@
+print("โปรเเกรมหาพื้นที่ 4 เหลี่ยมจัตุรัส")
+side = int(input("ใส่ค่าด้าน :"))
+square = side * side
+print(f"พื้นที่ 4 เหลี่ยมจัตุรัสนี้ คือ  {square}")
+
+
+print("โปรเเกรมหาพื้นที่ 4 เหลี่ยมผืนผ้า")
+wide = int(input("ใส่ค่ากว้าง :"))
+long = int(input("ใส่ค่ายาว :"))
+rectangle = wide * long
+print(f"พื้นที่ 4 เหลี่ยมผืนผ้านี้ คือ {rectangle}")
+
+
+print("โปรเเกรมหาพื้นที่ วงกลม")
+radius = int(input("ใส่รัศมี :"))
+circle = 3.14 * radius **2 
+print(f"พื้นที่วงกลมนี้ คือ {circle}")
